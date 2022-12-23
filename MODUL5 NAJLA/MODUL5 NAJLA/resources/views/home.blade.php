c5efcc1a0d07f47d1fcb1223ea48bcf717f0eab2
najlalya <najlalya@student.telkomuniversity.ac.id>
2022-12-23 15:36:11 +0700

    modul 5

---
+++ MODUL5 NAJLA/MODUL5 NAJLA/resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+
+@section('content')
+<!-- Jumbotron -->
+<section id="home">
+    <div class="container">
+        <div class="d-flex gap-5 wrap justify-content-center align-items-center">
+            <div>
+                <h1>Welcome To<br> Najla Show Room</h1>
+                <p class="mt-3">Najla's Show Room is a most successful showroom<br>ever in this universe</p>
+                <div class="d-flex  justify-content-lg-start mt-2">
+
+                </div>
+                <div class="d-flex align-items-center gap-5 mt-5">
+                    <img src="assets/img/logo-ead.png" alt="logoead" style="width:100px;">
+                    <p style="margin-top: 20px; font-size:14px;">Najla_1202204322</p>
+                </div>
+            </div>
+            <img src="assets/img/jumbotron.png" alt="">
+        </div>
+    </div>
+</section>
+<!-- Jumbotron End -->
+@endsection
